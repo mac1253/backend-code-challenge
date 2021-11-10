db972dcc2383ae330428078901c19b407d13a4c2
mac1253 <michiocorrigan@gmail.com>
2021-11-10 18:02:18 -0500

    Added get API and managers model

--- backend-code-challenge/Program.cs
+++ backend-code-challenge/Program.cs
@@ -1,6 +1,49 @@
+using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+
+app.MapGet("/api/supervisors", async () =>
+{
+    var request = new HttpRequestMessage(HttpMethod.Get, "https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers");
+    var client = new HttpClient();
+
+    var response = await client.SendAsync(request);
+    var managers = JsonSerializer.Deserialize<List<Managers>>(response.Content.ReadAsStreamAsync().Result);
+
+    List<String> results = new List<String>();
+    // Order the list by jurisdiction then lastname and then firstname
+    managers = managers.OrderBy(x => x.jurisdiction).ThenBy(x => x.lastName).ThenBy(x => x.firstName).ToList();
+
+    foreach (var manager in managers.ToList())
+    {
+        int i = 0;
+        // Remove managers with numerical jurisdictaion 
+        if (int.TryParse(manager.jurisdiction, out i))
+        {
+            managers.Remove(manager);
+            //Skip to next manager
+            continue;
+        }
+        // Add formatted string to results' list
+        results.Add($"{manager.jurisdiction} - {manager.lastName}, {manager.firstName}");
+    }
+
+    return results;
+
+});
+
 app.Run();
+
+
+public class Managers
+{
+    public string id { get; set; }
+    public string phone { get; set; }
+    public string jurisdiction { get; set; }
+    public string identificationNumber { get; set; }
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+}
